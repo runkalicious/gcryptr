bc3e3ccaa87efa3e91f37b4a4c439f30b88aa0cb
Matt Runkle <matt@mrunkle.com>
2015-07-18 18:53:10 -0400

    Setup interface: inject button into DOM for encryption

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
-var gmail;
+var global_compose;
 
-
-function refresh(f) {
-  if( (/in/.test(document.readyState)) || (undefined === Gmail) ) {
-    setTimeout('refresh(' + f + ')', 10);
-  } else {
-    f();
-  }
-}
-
-
-var main = function(){
-  // NOTE: Always use the latest version of gmail.js from
-  // https://github.com/KartikTalwar/gmail.js
-  gmail = new Gmail();
-  console.log('Hello,', gmail.get.user_email())
-  
-  gmail.observe.on("compose", function(compose, type) {
-
-      // type can be compose, reply or forward
-      console.log('api.dom.compose object:', compose, 'type is:', type );  // gmail.dom.compose object
+(function() {
+    var gmail = new Gmail();
+    
+    console.log('Hello,', gmail.get.user_email());
+    
+    gmail.observe.on("compose", function(compose, type) {
+        // TODO remove
+        console.log('api.dom.compose object:', compose, 'type is:', type );
+        global_compose = compose;
+        
+        var encryptButton = $("<div class=\"T-I J-J5-Ji aoO T-I-ax7 L3\">Encrypt</div>").click(function() {
+            encryptMessage(compose);
+        });
+        
+        compose.find("div.aoO").before(encryptButton);
     });
-}
-
-
-refresh(main);
+    
+    function encryptMessage(composeWindow) {
+        console.log(composeWindow.body());
+    }
+    
+    function decryptMessage(email) {
+        console.log(email.body());
+    }
+    
+})();
