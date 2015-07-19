528f534b6d0b810d8a46c014d7fa9b60eecaf0d6
Matt Runkle <matt@mrunkle.com>
2015-07-18 22:27:33 -0400

    Enable encryption with hardcoded public key.

--- inject.js
+++ inject.js
@@ -7,6 +7,10 @@ var g = document.createElement('script');
 g.src = chrome.extension.getURL('lib/gmail.js');
 (document.head || document.documentElement).appendChild(g);
 
+var f = document.createElement('script');
+f.src = chrome.extension.getURL('lib/kbpgp-2.0.8-min.js');
+(document.head || document.documentElement).appendChild(f);
+
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('main.js');
 (document.head || document.documentElement).appendChild(s);
--- main.js
+++ main.js
@@ -5,24 +5,75 @@ var global_compose;
     
     console.log('Hello,', gmail.get.user_email());
     
+    /* Add button to all compose windows */
     gmail.observe.on("compose", function(compose, type) {
-        // TODO remove
-        console.log('api.dom.compose object:', compose, 'type is:', type );
-        global_compose = compose;
-        
-        var encryptButton = $("<div class=\"T-I J-J5-Ji aoO T-I-ax7 L3\">Encrypt</div>").click(function() {
-            encryptMessage(compose);
-        });
+        var encryptButton = $("<div>")
+            .addClass("T-I J-J5-Ji aoO T-I-ax7 L3")
+            .text("Encrypt")
+            .click(function() {
+                encryptMessage(compose);
+            });
         
         compose.find("div.aoO").before(encryptButton);
     });
     
+    gmail.observe.on("view_thread", function(thread) {
+        // required to enable "view_email" listener
+        console.log(thread);
+    });
+    
+    /* Add listener for when an email is opened */
+    gmail.observe.on("view_email", function(email) {
+        console.log("Viewing email");
+        var msg = $(email.body().replace(/<br>/g, "\n")).text();
+        
+        console.log(msg);
+        
+        if (msg.match("^-----BEGIN PGP")) {
+            var link = $("<a href=\"#\" />")
+                .text("Decrypt now").click(function() {
+                    decryptMessage(msg, email);
+                });
+            var banner = $("<div>")
+                .text("This email is encrypted.")
+                .append(link);
+            
+            email.dom().before(banner);
+        }
+    });
+    
     function encryptMessage(composeWindow) {
-        console.log(composeWindow.body());
+        var body = $(composeWindow.body()).text();
+        
+        /* Assign PGP public key */
+        kbpgp.KeyManager.import_from_armored_pgp({
+            armored: public_key
+        },
+        function(err, recipient) {
+            if(!err) {
+                var params = {
+                    msg: body,
+                    encrypt_for: recipient
+                };
+                
+                /* Encrypt the message */
+                kbpgp.box(params, function(err, result_string, result_buffer) {
+                    result_string = result_string.replace(/\n/g, "<br>");
+                    composeWindow.body(result_string);
+                });
+            }
+            else {
+                console.log("Error loading public key: " + err);
+            }
+        });
     }
     
-    function decryptMessage(email) {
-        console.log(email.body());
+    function decryptMessage(message, email) {
+        console.log(message);
+        
+        // do decryption
     }
     
 })();
+
+var public_key = "";
