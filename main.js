c75034f85cea94d269426947800dd091dbd3cee0
Matt Runkle <matt@mrunkle.com>
2015-07-19 10:34:09 -0400

    Encrypt and load private key with hardcoded keys

--- main.js
+++ main.js
@@ -69,11 +69,74 @@ var global_compose;
     }
     
     function decryptMessage(message, email) {
-        console.log(message);
+        //var form = $("<input id=\"files\" type=\"file\" name=\"files\" class=\"modal\" />");
+        //$(document.body).append(form);
+        //$(form).jmodal();
+        
+        //var private_key = prompt("Please copy your private key here");
+        
+        kbpgp.KeyManager.import_from_armored_pgp({
+            armored: private_key
+        }, function(err, alice) {
+            if (!err) {
+                if (alice.is_pgp_locked()) {
+                    var passphrase = prompt("Please enter your password");
+                    
+                    alice.unlock_pgp({
+                        passphrase: passphrase
+                    }, function(err) {
+                        if (!err) {
+                            console.log("Loaded private key with passphrase");
+                        }
+                    });
+                } else {
+                    console.log("Loaded private key w/o passphrase");
+                }
+            }
+            else {
+                console.log("Error opening private key: " + err);
+            }
+        });
+        
+        /*document.getElementById('files').addEventListener('change', function(event) {
+            var files = event.target.files;
+            var r = new FileReader();
+            r.readAsBinaryString(files[0]);
+            r.onloadend = function(file) {
+                var buffer = new kbpgp.Buffer(r.result);
+                
+                
+                
+            };
+        }, false);*/
         
         // do decryption
+        /*var ring = new kbpgp.keyring.KeyRing;
+        var kms = [alice, bob, charlie];
+        var pgp_msg = "---- BEGIN PGP MESSAGE ----- ....";
+        var asp = 
+        for (var i in kms) {
+            ring.add_key_manager(kms[i]);
+        }
+        kbpgp.unbox({keyfetch: ring, armored: pgp_msg, asp }, function(err, literals) {
+          if (err != null) {
+            return console.log("Problem: " + err);
+          } else {
+            console.log("decrypted message");
+            console.log(literals[0].toString());
+            var ds = km = null;
+            ds = literals[0].get_data_signer();
+            if (ds) { km = ds.get_key_manager(); }
+            if (km) {
+              console.log("Signed by PGP fingerprint");
+              console.log(km.get_pgp_fingerprint().toString('hex'));
+            }
+          }
+        });*/
     }
     
 })();
 
 var public_key = "";
+
+var private_key = ""
