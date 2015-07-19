067ab9b802a692fa04ff338d4a6da56a575cd89e
Matt Runkle <matt@mrunkle.com>
2015-07-19 10:32:25 -0400

    Add jquery-ui for dialog

--- inject.js
+++ inject.js
@@ -1,16 +1,32 @@
+var SCRIPTS = [
+    'lib/jquery-1.10.2.min.js',
+    'lib/jquery-ui.min.js',
+    'lib/gmail.js',
+    'lib/kbpgp-2.0.8-min.js',
+    'main.js'
+];
 
-var j = document.createElement('script');
-j.src = chrome.extension.getURL('lib/jquery-1.10.2.min.js');
-(document.head || document.documentElement).appendChild(j);
+var STYLES = [
+    'lib/jquery-ui.min.css',
+    'lib/jquery-ui.structure.min.css',
+    'lib/jquery-ui.theme.min.css'
+];
 
-var g = document.createElement('script');
-g.src = chrome.extension.getURL('lib/gmail.js');
-(document.head || document.documentElement).appendChild(g);
+function injectScript(filename, type) {
+    var s = document.createElement(type);
+    s.src = chrome.extension.getURL(filename);
+    s.onload = function() {
+        s.parentNode.removeChild(s);
+    };
+    (document.head || document.documentElement).appendChild(s);
+}
 
-var f = document.createElement('script');
-f.src = chrome.extension.getURL('lib/kbpgp-2.0.8-min.js');
-(document.head || document.documentElement).appendChild(f);
+// Inject javascript
+for (var i = 0, len = SCRIPTS.length; i < len; i++) {
+    function(SCRIPTS[i], "script");
+}
 
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('main.js');
-(document.head || document.documentElement).appendChild(s);
+// inject CSS
+for (var i = 0, len = STYLES.length; i < len; i++) {
+    function(STYLES[i], "style");
+}
